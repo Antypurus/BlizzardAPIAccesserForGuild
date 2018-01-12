86dfc31c3ed65d7f6706fae7d690c6995f7d8479
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-01-12 01:39:28 +0000

    initial commit
    Currently looks for Hunters,Rogues and Priest Between 915 iLvl and 930 iLvl

---
+++ Prototype.py
@@ -0,0 +1,54 @@
+import json
+import requests
+
+def sendResquest(url):
+	return requests.get(url)
+
+classMap = {0: "None",
+            1: "Warrior",
+            2: "Paladin",
+            3: "Hunter",
+            4: "Rogue",
+            5: "Priest",
+            6: "Death Knigth",
+            7: "Shaman",
+            8: "Mage",
+            9: "Warlock",
+            10: "Monk",
+            11: "Druid",
+            12: "Demon Hunter"
+            }
+
+base = "https://eu.api.battle.net/wow/character/"
+end = "?fields=items&locale=en_GB&apikey=54ynts6k49dsq5vqm85ecmask7gmjkqa"
+
+guildURI = "https://eu.api.battle.net/wow/guild/%20Aggra%20(Português)/For%20The%20Invicta?fields=members&locale=en_GB&apikey=54ynts6k49dsq5vqm85ecmask7gmjkqa"
+Resp = sendResquest(guildURI)
+j = json.loads(Resp.content)
+
+n = 0
+for i in j["members"]:
+	member = i.get("character")
+	level = member.get("level")
+	pClass = classMap.get(member.get("class"))
+	name = member.get("name")
+	realm = member.get("realm").replace(" ", "%20")
+
+	if level>=110 and pClass=="Hunter" or pClass=="Priest" or pClass=="Rogue":
+		res = sendResquest(base + realm + "/" + name + end)
+		p = json.loads(res.content)
+
+		ilvl = p.get("items").get("averageItemLevelEquipped")
+
+		if ilvl>=915 and ilvl<930:
+			n = n + 1
+			print("\tName:{}".format(name))
+			print("\tClass:{}".format(pClass))
+			print("\tLevel:{}".format(level))
+			print("\tItem Level:{}".format(ilvl))
+			print("")
+
+
+
+
+print("Number of Hunter:{}".format(n))
